271cb44395751287903cb9fe0f7cdf1264f53428
Elias Graal <shadowgt@mail.ru>
2023-05-18 21:06:19 +0300

    Update and add files

--- src/test/java/ru/netology/PositiveTest.java
+++ src/test/java/ru/netology/PositiveTest.java
@@ -14,17 +14,13 @@ public class PositiveTest {
     private String Date(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
-
-    @BeforeEach
-
-    void setup() {open("http://localhost:9999");}
     @Test
     void shouldSubmitValidData() {
         open("http://localhost:9999");
         $("[data-test-id=city] .input__control").setValue("Санкт-Петербург");
         String currentDate = Date(3, "dd.MM.yyyy");
         $("[data-test-id = date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id = date] .input__control").setValue("19.05.2023");
+        $("[data-test-id = date] .input__control").sendKeys(currentDate);
         $("[data-test-id = name] .input__control").setValue("Есенин Сергей");
         $("[data-test-id = phone] .input__control").setValue("+79117778899");
         $("[data-test-id = agreement]").click();
