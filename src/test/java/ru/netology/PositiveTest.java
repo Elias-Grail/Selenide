a6a6c143f076b0b94ffab31f34d8feea4d8159ae
Elias Graal <shadowgt@mail.ru>
2023-05-22 08:12:25 +0300

    Update build.gradle and PositiveTest

--- src/test/java/ru/netology/PositiveTest.java
+++ src/test/java/ru/netology/PositiveTest.java
@@ -3,7 +3,6 @@ package ru.netology;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
-
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -14,19 +13,36 @@ public class PositiveTest {
     private String Date(int days, String pattern) {
         return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern(pattern));
     }
+
     @Test
-    void shouldSubmitValidData() {
-        open("http://localhost:9999");
-        $("[data-test-id=city] .input__control").setValue("Санкт-Петербург");
-        String currentDate = Date(3, "dd.MM.yyyy");
-        $("[data-test-id = date] .input__control").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id = date] .input__control").sendKeys(currentDate);
-        $("[data-test-id = name] .input__control").setValue("Есенин Сергей");
-        $("[data-test-id = phone] .input__control").setValue("+79117778899");
+    void validFormCompletion() {
+        open ("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Санкт-Петербург");
+        String currentDate = Date(5, "dd.MM.yyyy");
+        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id = date] input").sendKeys(currentDate);
+        $("[data-test-id = name] input").setValue("Есенин Сергей");
+        $("[data-test-id = phone] input").setValue("+79117778899");
         $("[data-test-id = agreement]").click();
         $(".button").click();
         $(".notification__content")
                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+
+    }
+    @Test
+    void threeHundredSixtyFive() {
+        open ("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("Москва");
+        String currentDate = Date(365, "dd.MM.yyyy");
+        $("[data-test-id = date] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id = date] input").sendKeys(currentDate);
+        $("[data-test-id = name] input").setValue("Есенин Сергей");
+        $("[data-test-id = phone] input").setValue("+79117778899");
+        $("[data-test-id = agreement]").click();
+        $(".button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
 
     }
